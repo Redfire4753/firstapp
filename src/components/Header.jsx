72f20ecc82d75d21a339bd8b8bdcdf241a18c6f9
Ben Lawton <lawtonb16@gmail.com>
2022-09-24 23:51:24 -0500

    formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 const HeaderText = styled.h1`
     margin: 0;
-    color: rgba(255,202,53,255);
+    color: rgba(255, 202, 53, 255);
     text-shadow: 2px 2px 3px brown;
 `;
 const HeaderWrapper = styled.header`
-    background: linear-gradient(45deg, rgba(28,17,24,255) 0%, rgba(29,17,21,255) 50%, rgba(132,32,22,255) 100%);
+    background: linear-gradient(
+        45deg,
+        rgba(28, 17, 24, 255) 0%,
+        rgba(29, 17, 21, 255) 50%,
+        rgba(132, 32, 22, 255) 100%
+    );
     width: 100vw;
     height: 100px;
     display: flex;
@@ -16,11 +21,9 @@ const HeaderWrapper = styled.header`
 const Header = () => {
     return (
         <HeaderWrapper>
-            <HeaderText>
-                Vampire Survivors
-            </HeaderText>
+            <HeaderText>Vampire Survivors</HeaderText>
         </HeaderWrapper>
-    )
-}
+    );
+};
 
 export default Header;
